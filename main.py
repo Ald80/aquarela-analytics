f588da5bb6bf4654161ce9387d095ab4a970b57a
Aldrick <aldrick.kronos@gmail.com>
2025-01-06 20:47:58 -0300

    included colaborador routes in fastapi app

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from routes import colaborador
-
+from routes.colaborador.create import router  as colaborador_create_route 
+from routes.colaborador.delete import router as colaborador_delete_route 
+from routes.colaborador.update import router as colaborador_update_route 
+from routes.colaborador.read import router as colaborador_read_route 
 
 app = FastAPI()
-app.include_router(colaborador.router)
-
-# api_router = APIRouter()
-# api_router.include_router(colaborador.router)
-# api_router.include_router()
+app.include_router(colaborador_create_route)
+app.include_router(colaborador_delete_route)
+app.include_router(colaborador_update_route)
+app.include_router(colaborador_read_route)
